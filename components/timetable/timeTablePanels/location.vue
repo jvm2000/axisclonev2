ff709a293674bf6de57d3b623f2e3430c4523138
jvm2000 <minozajohnvincent2000@gmail.com>
2023-05-30 20:28:29 +0800

    swol

--- components/timetable/timeTablePanels/location.vue
+++ components/timetable/timeTablePanels/location.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
     import { PlusIcon, XMarkIcon, ChevronRightIcon, ChevronDownIcon } from '@heroicons/vue/24/outline'
-    const { locationList, add, locationN, locationObj, objL, onClass } = addLocation()
+    const { locationList, add, locationN, locationObj, objL, onClass, classN, classAdd, onClassInput, classDel, isUpdate, updateObj, locationUp, nameObj, updateLocation } = addLocation()
 
     const isAddLocation = ref(false)
     const isClassLocation = ref(false)
@@ -16,6 +16,7 @@
 
 
 </script>
+
 <template>
     <div class="py-14 w-full">
         <div class="w-full flex space-x-6">
@@ -36,16 +37,120 @@
                 <p class="absolute right-[7rem]">CLASS AREA</p>
             </div>
             <div 
-                class="w-full px-2 py-3 bg-white rounded-lg drop-shadow-sm place-items-center text-[16px] flex relative"
+                class="w-full h-auto px-2 py-3 bg-white rounded-lg drop-shadow-sm text-[16px] flex flex-col relative"
                 v-for="(locals, index) in locationList"
                 :key="index"
             >
-                <div class="flex place-items-center w-[44rem]">
-                    <ChevronRightIcon class="w-3 h-3 text-gray-400 mr-2"/>
-                    <p class="text-[16px]">{{ locals.name }}</p>
+                <div 
+                    v-if="locals.locUpdate"
+                    class="h-auto w-full px-2 py-1 flex place-items-center space-x-1"
+                >
+                    <ChevronRightIcon class="w-3 h-3 text-gray-400"/>
+                    <div class="w-full h-[2rem] flex place-items-center relative rounded-sm border-2">
+                        <input 
+                            type="text"
+                            class="outline-none w-[20rem] px-4"
+                            v-model="locationUp"
+                            @keyup.enter="objL(locals),updateLocation(locals.name), isUpdate(false)"
+                        >
+                        <XMarkIcon 
+                            class="w-4 h-4 text-[#9FB5D1] cursor-pointer absolute right-4 mt-1"
+                            @click="objL(locals),isUpdate(false)"
+                        />
+                    </div>
                 </div>
-                <p class="text-[16px]">{{ locals.classes.length }}</p>
-                <XMarkIcon class="w-4 h-4 text-[#9FB5D1] cursor-pointer absolute right-4"/>
+
+                <div 
+                    v-else
+                    class="flex place-items-center relative"
+                >
+                    <div 
+                        class="flex w-[44rem] place-items-center cursor-pointer"
+                    >
+                        <ChevronRightIcon 
+                            v-if="!locals.classOn"
+                            class="w-3 h-3 text-gray-400 mr-2 cursor-pointer"
+                            @click="objL(locals), onClass(true)"
+                        />
+                        <ChevronDownIcon 
+                            v-else
+                            class="w-3 h-3 text-gray-400 mr-2 cursor-pointer"
+                            @click="objL(locals), onClass(false)"
+                        />
+                        <p 
+                            class="text-[16px]"
+                            @click="objL(locals),isUpdate(true),updateObj(locals.name)"
+                        >
+                            {{ locals.name }}
+                        </p>
+                    </div>
+                    <p class="text-[16px]">
+                        {{ locals.classes.length }}
+                    </p>
+                    <XMarkIcon class="w-4 h-4 text-[#9FB5D1] cursor-pointer absolute right-4 mt-1"/>
+                </div>
+
+                <div
+                    v-if="locals.classOn"
+                    class="mt-4 w-full relative px-2"
+                >
+                <p class="text-sm text-[#9FB5D1] font-semibold mt-4 m-5">CLASS AREA</p>
+
+                    <div 
+                        class="relative bg-[#F1F6FC] h-auto space-y-4 grid rounded-lg"
+                        :class="[locals.classes.length > 0 ? 'py-2' : '']"
+                    >
+                        <div 
+                            v-for="classess, index in locals.classes"
+                            :key="index"
+                            class="w-full py-1 place-items-center border-b-2 border-[#E9F0FA]"
+                        >
+                            <div class="pl-4 flex place-items-center space-x-4 h-[2rem] text-sm relative">
+                                <span class="w-2 h-2 bg-[#46DBA8]"></span>
+                                <p>{{ classess.name }}</p>
+                                <XMarkIcon 
+                                    class="w-4 h-4 text-[#5E6E82] absolute right-6 cursor-pointer"
+                                    @click="objL(classess),classDel(index)"
+                                />
+                            </div>
+                        </div>
+                        <div 
+                            v-if="locationObj.classInput"
+                            class="w-full"
+                            :class="locals.classes.length > 0 ? 'py-2' : 'py-6'"
+                        >
+                            <div 
+                                class="w-11/12 h-[2.5rem] rounded-lg border-2 drop-shadow-lg bg-[#F1F6FC] flex relative border-[#5081F0] place-items-center ml-8"
+                            >
+                                <input 
+                                    type="text"
+                                    class="w-6/12 text-sm outline-none bg-[#F1F6FC] pl-4"
+                                    v-model="classN"
+                                    @keyup.enter="objL(locals),classAdd(locals.classes)"
+                                >
+                                <XMarkIcon
+                                    class="w-4 h-4 text-[#9FB5D1] absolute right-4 cursor-pointer"
+                                    @click="objL(locals), onClassInput(false)"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full relative h-[3rem]">
+                        <div 
+                            class="flex space-x-4 absolute right-4 pt-4 place-items-center font-semibold"
+                            :class="locationObj.classInput ? 'text-[#CAD7E8]' : 'text-[#5081F0] cursor-pointer'"
+                        >
+                            <PlusIcon class="w-4 h-4"/>
+                            <button
+                                :disabled="locationObj.classInput"
+                                @click="objL(locals), onClassInput(true)"
+                            >
+                                Add Class Area
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div 
                 v-if="isAddLocation"
--- composables/addLocation.ts
+++ composables/addLocation.ts
@@ -1,7 +1,10 @@
 const state = reactive({
     locationList: [],
     locationN: '',
+    locationUp: '',
+    classN: '',
     locationObj: {},
+    nameObj: {},
     objName: {},
     isDup: false,
     i: 0,
@@ -12,6 +15,30 @@ export default function () {
 
     const objL = (locals: Object) => {
         state.locationObj = locals
+    }
+
+    const updateObj = (name: Object) => {
+        state.nameObj.name = name
+        state.locationUp = state.nameObj.name
+    }
+
+    const classAdd = (classes: Object) => {
+        if(state.classN == '')
+            return alert('Invalid! Input should not be empty.')
+
+        state.locationObj.classes = classes
+
+        let classesObj = {
+            name: state.classN,
+        }
+
+        state.locationObj.classes.push(classesObj)
+        state.classN = ''
+
+    }
+
+    const classDel = (index) => {
+        state.locationObj.classes.splice(index, 1)
     }
 
     const add = () => {
@@ -29,13 +56,29 @@ export default function () {
             name: state.locationN,
             classes: [],
             classOn: false,
+            classInput: false,
+            locUpdate: false
         }
         state.locationList.push(location)
         state.locationN = ''
     }
 
+    const updateLocation = (name: Object) => {
+        state.locationObj.name = name
+        state.locationObj.name = state.locationUp
+        state.locationUp = ''
+    }
+
     const onClass = (classOn: Boolean) => {
         state.locationObj.classOn = classOn
+    }
+    
+    const isUpdate = (locUpdate: Boolean) => {
+        state.locationObj.locUpdate = locUpdate
+    }
+
+    const onClassInput = (classInput: Boolean) => {
+        state.locationObj.classInput = classInput
     }
 
     return{
@@ -43,5 +86,11 @@ export default function () {
         add,
         objL,
         onClass,
+        classAdd,
+        onClassInput,
+        classDel,
+        updateLocation,
+        isUpdate,
+        updateObj,
     }
 }
